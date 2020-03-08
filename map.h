3320758fb2f0b07ca8e58b500960b852839098d9
user <user@email.com>
2020-03-08 21:07:52 +0000

    start working on player

--- map.h
+++ map.h
@@ -7,28 +7,56 @@
 #include "types.h"
 
 typedef struct map{
-    int dim;
     SHIP **ships;
+    TILE **matrix; // 0 -> EMPTY / 1 -> FILLED / 2 -> ALREADY HIT
 }MAP;
 
 //---------------------------------------------------
-static void map_exit_error(char *);
+MAP *create_map(SHIP **, TILE **);
+
+TILE **create_empty_map(int);
 
 void free_map(MAP *);
 
+static void map_error(char *);
+
 //---------------------------------------------------
-static void map_exit_error(char *msg){
-    fprintf(stderr,"Error: %s.\n",msg);
-    exit(EXIT_FAILURE);
+TILE **create_empty_map(int dim){
+    TILE **matrix;
+    matrix = malloc(sizeof(TILE*)*dim);
+    for(int i=0; i<dim; i++){
+        matrix[i] = malloc(sizeof(TILE*)*dim);
+    }
+    //initialize all the positions as 0 (empty)
+    for(int i=0; i<dim; i++){
+        for(int j=0; j<dim; j++){
+            matrix[i][j] = EMPTY;
+        }
+    }
+    return matrix;
+}
+
+MAP *create_map(SHIP **ships, TILE **matrix){
+    MAP *m = (MAP*)malloc(sizeof(MAP));
+    if(m == NULL){map_error("No memory");}
+    m -> ships = ships;
+    m -> matrix = matrix;
+    return m;
 }
 
 void free_map(MAP *m){
     if(m != NULL){
         free(m -> ships);
+        free(m -> matrix);
         free(m);
     }else{
-        map_exit_error("The map was badly generated.");
+        map_error("The map was badly generated.");
     }
+}
+
+static void map_error(char *msg){
+    fprintf(stderr,"Error: %s.\n",msg);
+    exit(EXIT_FAILURE);
 }
 
 #endif
--- ship.h
+++ ship.h
@@ -10,36 +10,22 @@ typedef struct ship{
     int size;
     int hits;   // how many attacks suffered
     BOOL sunk; // FALSE -> alive / TRUE -> dead
+    DIR dir;    // 0 -> HORIZONTAL / 1 -> VERTICAL
 }SHIP;
 
-//---------------------------------------------------------------
-static void ship_error(char *);
-
-static int calculate_size(COORD, COORD);
+//--------------------------------------------------------------
 
 SHIP *create_ship(COORD, COORD);
 
-void free_ship(SHIP *);
-
 void set_size(SHIP *, COORD, COORD);
 
+static int calculate_size(SHIP *, COORD, COORD);
+
+void free_ship(SHIP *);
+
+static void ship_error(char *);
+
 //---------------------------------------------------------------
-
-static int calculate_size(COORD begin, COORD end){
-    if(begin.x == end.x){ //Vertical
-        return end.y - begin.y;
-    }
-    else if(begin.y == end.y){ //Horizontal
-        return end.x - begin.x;
-    }
-    return -1;
-}
-
-//Call this when there is a memory related error
-static void ship_error(char *msg){
-    fprintf(stderr,"Error: %s.\n",msg);
-    exit(EXIT_FAILURE);
-}
 
 SHIP *create_ship(COORD begin, COORD end){ // VERIFICAR COORDENANDAS CORRECTAS
     SHIP *s = (SHIP *)malloc(sizeof(SHIP));
@@ -53,7 +39,7 @@ SHIP *create_ship(COORD begin, COORD end){ // VERIFICAR COORDENANDAS CORRECTAS
 }
 
 void set_size(SHIP *s, COORD begin, COORD end){
-    int size = calculate_size(begin, end);
+    int size = calculate_size(s, begin, end);
     if (size == -1){
         ship_error("Invalid ship coordinates");
         free_ship(s);
@@ -63,6 +49,18 @@ void set_size(SHIP *s, COORD begin, COORD end){
     }
 }
 
+static int calculate_size(SHIP *s, COORD begin, COORD end){
+    if(begin.x == end.x){ //Vertical
+        s -> dir = VERTICAL;
+        return end.y - begin.y;
+    }
+    else if(begin.y == end.y){ //Horizontal
+        s -> dir = HORIZONTAL;
+        return end.x - begin.x;
+    }
+    return -1;
+}
+
 //Destroys the structure
 void free_ship(SHIP *s){
     if(s != NULL){
@@ -70,6 +68,12 @@ void free_ship(SHIP *s){
     }else{
         ship_error("The ship was badly generated");
     }
+}
+
+//Call this when there is a memory related error
+static void ship_error(char *msg){
+    fprintf(stderr,"Error: %s.\n",msg);
+    exit(EXIT_FAILURE);
 }
 
 #endif
